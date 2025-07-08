75c094b89aaf7ddcc7ea49ba4c56ed66b0cc6b9b
Kambhampati-Sruthi <kambhampatisruthi@gmail.com>
2025-07-08 19:36:59 +0530

    Update app0.py

--- app0.py
+++ app0.py
@@ -1,7 +1,34 @@
 import os
-import gdown
+import requests
+def download_file_from_google_drive(file_id, destination):
+    URL = "https://drive.google.com/uc?export=download"
+    session = requests.Session()
+    response = session.get(URL, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+    with open(destination, 'wb') as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
+
+# Download only if not already present
 if not os.path.exists("leafdiseasemodel.keras"):
-    gdown.download("https://drive.google.com/uc?id=11EZDuzAm8zFxteYhrPRDzub42wmTdFD_", "leafdiseasemodel.keras", quiet=False)
+    file_id = "11EZDuzAm8zFxteYhrPRDzub42wmTdFD_"
+    download_file_from_google_drive(file_id, "leafdiseasemodel.keras")
 import streamlit as st
 import numpy as np
 import tensorflow as tf
